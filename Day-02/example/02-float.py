962bbbb890b7dffbd8aebccf5b78c0cabeea1c7f
pratikgondkar <pratikgondkar4040@gmail.com>
2025-12-06 13:57:31 +0530

    adding the float programer

---
+++ Day-02/example/02-float.py
@@ -0,0 +1,17 @@
+num1 = 5.2
+num2 = 2.5
+
+add = num1 + num2
+print("Addition:", add)
+
+sub = num1 - num2
+print("subsraction:", sub)
+
+mul = num1 * num2
+print("multiplication:", mul)
+
+div = num1 / num2
+print("Division:", div)
+
+rounded_value = round(3.56789, 2)
+print("rounded_value:", rounded_value)
