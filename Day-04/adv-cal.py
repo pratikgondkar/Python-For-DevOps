00c3d4cab10d3edcb356b7bf1d9721f9e83861b8
pratikgondkar <pratikgondkar4040@gmail.com>
2025-12-07 15:51:35 +0530

    adding some function and module programe

---
+++ Day-04/adv-cal.py
@@ -0,0 +1,4 @@
+import cal as calculator
+
+add_result = calculator.addition(20, 10)
+print(f"Addition Result: {add_result}")
---
+++ Day-04/cal.py
@@ -0,0 +1,20 @@
+def addition (num1, num2):
+    add = num1 + num2
+    return add
+
+def subtraction (num1, num2):
+    sub = num1 - num2
+    return sub 
+
+def multiplication (num1, num2):
+    mul = num1 * num2
+    return mul 
+
+def division (num1, num2):
+    div = num1 / num2
+    return div 
+
+print(addition(10, 5))
+print(subtraction(10, 5))
+print(multiplication(10, 5))
+print(division(10, 5))
