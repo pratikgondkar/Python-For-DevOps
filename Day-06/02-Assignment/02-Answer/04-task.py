650cd2955a8746b1a314f62e454dc1da66c3e6b9
pratikgondkar <pratikgondkar4040@gmail.com>
2025-12-09 18:49:25 +0530

    adding task three programe

---
+++ Day-06/02-Assignment/02-Answer/04-task.py
@@ -0,0 +1,13 @@
+total = 10
+
+total += 5
+print("After adding 5, total is:", total)
+
+total -= 3
+print("After subtracting 3, total is:", total)  
+
+total *= 2
+print("After multiplying by 2, total is:", total)
+
+total /= 4
+print("After dividing by 4, total is:", total)
