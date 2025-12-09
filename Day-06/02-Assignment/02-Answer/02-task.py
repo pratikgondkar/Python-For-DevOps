ef13caf8f8926d9500fcdc3b2c4fb3e1e7b805f6
pratikgondkar <pratikgondkar4040@gmail.com>
2025-12-09 18:41:40 +0530

    adding task two programe

---
+++ Day-06/02-Assignment/02-Answer/02-task.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+
+less_than = a < b
+print(a, "is less than", b, ":", less_than)
+
+greater_than = a > b
+print(a, "is greater than", b, ":", greater_than)
+
+less_than_equal = a <= b
+print(a, "is less than or equal to", b, ":", less_than_equal)
+
+greater_than_equal = a >= b
+print(a, "is greater than or equal to", b, ":", greater_than_equal)
+
+equal_to = a == b
+print(a, "is equal to", b, ":", equal_to)
+
+not_equal_to = a != b
+print(a, "is not equal to", b, ":", not_equal_to)
