31c714ae696d4efacc41972b8476053e47379c2a
pratikgondkar <pratikgondkar4040@gmail.com>
2025-12-09 18:37:40 +0530

    adding task one programe

---
+++ Day-06/02-Assignment/02-Answer/01-task.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+sum_result = a + b
+print("The sum of", a, "and", b, "is:", sum_result)
+
+diff_result = a - b
+print("The difference when", b, "is subtracted from", a, "is:", diff_result)    
+
+prod_result = a * b
+print("The product of", a, "and", b, "is:", prod_result)
+
+div_result = a / b
+print("The division of", a, "by", b, "gives:", div_result)
