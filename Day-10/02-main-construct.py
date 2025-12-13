1fe2f7053609a9ca4e0725fa48bf7813143232e7
pratikgondkar <pratikgondkar4040@gmail.com>
2025-12-13 14:13:15 +0530

    adding the main construct

---
+++ Day-10/02-main-construct.py
@@ -0,0 +1,11 @@
+def main():
+    folder_path = input("Enter the folder path separated by space: ").split()
+    print(folder_path)
+
+
+       # Print elements in the list
+       #for folder_path in folder_paths:
+       #    print(folder_path)
+
+if __name__ == "__main__":
+    main()
